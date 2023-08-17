0f106c1f0a105ca1a2d714297be14e21b813baec
igor.guariroba <igor.guariroba@jsl.com.br>
2023-08-17 01:38:01 -0300

    Adiciona validação de cabeçalho para conversão CSV
    Este commit introduz uma nova funcionalidade no módulo File que valida o cabeçalho de um arquivo CSV antes de convertê-lo em JSON. Se o cabeçalho não for válido, um erro será lançado. Além disso, foi adicionado um novo arquivo CSV para testar a validade da implementação e também foi criado um novo módulo para armazenar mensagens de erro constantes.

---
+++ mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+    error: {
+        FILE_LENGTH_ERROR_MESSAGE: 'O tamanho não é valido!',
+        FILE_FIELDS_ERROR_MESSAGE: 'As propriedade não são validas!'
+    }
+}
+
+module.exports = constants
--- mocks/src/file.js
+++ mocks/src/file.js
@@ -1,8 +1,16 @@
-const { readFile } =  require('fs/promises')
-const { join } = require('path')
+const {readFile} = require('fs/promises')
+const {join} = require('path')
+const {error} = require('./constants')
+const DEFAULT_OPTIONS = {
+    maxLines: 3,
+    fields: ["id", "name", "profession", "age"]
+}
+
 class File {
-    static async csvToJson(filePath){
+    static async csvToJson(filePath) {
         const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
+        if (!validation.valid) throw new Error(validation.error)
         return content
     }
 
@@ -10,9 +18,23 @@ class File {
         const filename = join(__dirname, filePath)
         return (await readFile(filename)).toString("utf8")
     }
+
+    static isValid(csvString, options = DEFAULT_OPTIONS) {
+        const [header, ...fileWithoutHeader] = csvString.split('\n')
+        const isHeaderValid = header === options.fields.join(',')
+        if (!isHeaderValid) {
+            return {
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+    }
 }
 
-(async () =>{
-    const result = await File.csvToJson('./../mocks/threeltems-valid.csv')
-    console.log('result',result)
+(async () => {
+    // const result = await File.csvToJson('./../mocks/threeltems-valid.csv')
+    // const result = await File.csvToJson('./../mocks/fourltems-valid.csv')
+    // const result = await File.csvToJson('./../mocks/fourltems-valid.csv')
+    const result = await File.csvToJson('./../mocks/invalid-header.csv')
+    console.log('result', result)
 })();
