7110aefc2edded847fae0d97783ebebe0105875c
igor.guariroba <igor.guariroba@jsl.com.br>
2023-08-26 17:53:16 -0300

    "Adiciona setup inicial ao teste do CarService e importa módulo no BaseRepository
    O carService.test.js foi modificado para incluir o setup inicial do teste unitário para o CarService, incluindo a criação de um novo CarService com o arquivo de base de dados car.json.
    Além disso, a função 'before' de Mocha foi usada para executar este setup antes de cada teste.
    Na baseRepository.js, foi importado o módulo 'fs/promises' para permitir a leitura de arquivos de forma assíncrona. Esta mudança é necessária para acessar o banco de dados em car.json."

--- project-tdd-2/test/unitTests/carService.test.js
+++ project-tdd-2/test/unitTests/carService.test.js
@@ -1,7 +1,20 @@
-const {describe,it} = require('mocha')
+const {describe, it, before} = require('mocha')
+const CarService = require('./../../src/service/carService')
 
+const {join} = require('path')
+
+const carDatabase = join(__dirname, './../../database', "car.json")
 describe('CarService suite tests', () => {
-    it('should test!', () => {
-        
+    let carService = {}
+
+    before(() => {
+        carService = new CarService({
+            cars: carDatabase
+        })
+    })
+
+    it('given a carCategory it should return an available car', async () => {
+        const result = await carService.test()
+        console.log('result', result)
     });
 })
