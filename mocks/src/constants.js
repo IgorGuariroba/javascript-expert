1fdc1a093dc95005f63b7bf66f760e671d2a57c8
igor.guariroba <igor.guariroba@jsl.com.br>
2023-08-27 14:56:37 -0300

    Remove redundant test and project files
    This commit removes redundant .nycrc.json, api.js, api.test.js, base.js, baseRepository.js, car.js, car.json, carCategories.json, carCategory.js, and carService.js files from the coverage-e2e and project-tdd subdirectories. These files were no longer necessary after streamlining the project structure and improving the organization of test cases.

--- mocks/src/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-    error: {
-        FILE_LENGTH_ERROR_MESSAGE: 'O tamanho não é valido!',
-        FILE_FIELDS_ERROR_MESSAGE: 'As propriedade não são validas!'
-    }
-}
-
-module.exports = constants
